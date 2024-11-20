27d8d4b4d41803c4fe147bcb8c1b8931815a74cd
CherniIyes <cherniiyes@gmail.com>
2024-11-20 14:15:07 +0100

    LAID OFF

--- my-app/app/pages/Home/page.jsx
+++ my-app/app/pages/Home/page.jsx
@@ -1,6 +1,6 @@
 import CoinSearch from '@Components/CoinSearch'
 import Trending from '@Components/Trending'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Home = ({ coins }) => {
       return (
