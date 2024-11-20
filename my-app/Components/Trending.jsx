4343bb6180639ea632f5d958002542188e2a9ef5
CherniIyes <cherniiyes@gmail.com>
2024-11-20 14:36:34 +0100

    LAID OFF

--- my-app/Components/Trending.jsx
+++ my-app/Components/Trending.jsx
@@ -1,56 +1,59 @@
-"use client"
+"use client";
 import axios from 'axios';
-import Link from 'next/link';
-import React, { useEffect, useState } from 'react'
-
+import React, { useEffect, useState } from 'react';
 
 const Trending = () => {
       const [trending, setTrending] = useState([]);
-
+      const [isLoading, setIsLoading] = useState(true);
 
       const url = 'https://api.coingecko.com/api/v3/search/trending';
-      // Fetch Trending Coins Data from API
 
       useEffect(() => {
-            axios.get(url).then((response) => {
-                  setTrending(response.data.coins);
-                  // console.log("response Coins", response.data.coins);
-            })
-      }, [])
+            axios.get(url)
+                  .then((response) => {
+                        setTrending(response.data.coins);
+                        setIsLoading(false);
+                  })
+                  .catch((error) => {
+                        console.error("Error fetching data:", error);
+                        setIsLoading(false);
+                  });
+      }, []);
 
-
-      // console.log(cName);
       return (
+            <div className="rounded-div my-12 py-8 text-primary">
+                  <h1 className="text-2xl font-bold pb-5 px-4">Trending Flous</h1>
 
+                  {isLoading ? (
+                        <p className="text-center">Loading...</p>
+                  ) : (
+                        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+                              {trending.map((coin) => (
+                                    <div
+                                          key={coin.item.id}
+                                          className="rounded-div flex items-center justify-between p-4 hover:scale-105 ease-in-out duration-300"
+                                    >
+                                          <div className="flex items-center">
+                                                <img
+                                                      className="rounded-full"
+                                                      src={coin.item.small}
+                                                      alt={coin.item.name}
+                                                />
+                                                <div className="flex-col items-center mx-4">
+                                                      <p className="font-bold">{coin.item.name}</p>
+                                                      <p>{coin.item.symbol}</p>
+                                                </div>
+                                          </div>
 
-            <div className='rounded-div my-12 py-8 text-primary '>
-
-                  <h1 className='text-2xl font-bold pb-5 px-4'>Trending Flous</h1>
-
-
-                  <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-4'>
-                        {trending.map((coin) => (
-                              <div className='rounded-div flex items-center justify-between p-4 hover:scale-105 ease-in-out duration-300' >
-
-                                    <div className='flex items-center'>
-                                          <img className='rounded-full' src={coin.item.small} alt="/" />
-                                          <div className='flex-col items-center mx-4'>
-                                                <p className='font-bold' >{coin.item.name}</p>
-                                                <p>{coin.item.symbol}</p>
+                                          <div className="flex items-center justify-between mr-2">
+                                                <p>${coin.item.price_btc.toFixed(7)}</p>
                                           </div>
                                     </div>
-
-
-                                    <div className='flex items-center justify-between mr-2'>
-                                          <img className='w-5 mr-2 rounded-full' src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579" alt="" />
-                                          <p>${coin.item.price_btc.toFixed(7)}</p>
-                                    </div>
-                              </div>
-                        ))}
-                  </div>
+                              ))}
+                        </div>
+                  )}
             </div>
-      )
+      );
+};
 
-}
-
-export default Trending
+export default Trending;
