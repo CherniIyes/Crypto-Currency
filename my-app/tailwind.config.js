eec33d6c9f16f19042e4986eaf9eecaa7275ee29
CherniIyes <cherniiyes@gmail.com>
2024-11-22 11:58:08 +0100

    polishing

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export const content = [
   "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-  "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  "./Components/**/*.{js,ts,jsx,tsx,mdx}",
   "./app/**/*.{js,ts,jsx,tsx,mdx}",
 ];
 export const theme = {
