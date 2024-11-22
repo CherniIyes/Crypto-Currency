88e7dc18a94bd2be626f5755efb4ae53eab045f6
CherniIyes <cherniiyes@gmail.com>
2024-11-22 11:52:26 +0100

    polishing

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -1,33 +1,31 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundColor: {
-        primary: 'var(--color-bg-primary)',
-        // added
-        primaryy: 'var(--color-bg-primaryy)',
-        // added
-        secondary: 'var(--color-bg-secondary)',
-        button: 'var(--color-bg-button)',
-      },
-      textColor: {
-        accent: 'var(--color-text-accent)',
-        primary: 'var(--color-text-primary)',
-        secondary: 'var(--color-text-secondary)',
-        btnText: 'var(--color-bg-secondary)',
-      },
-      borderColor: {
-        primary: 'var(--color-bg-primary)',
-        secondary: 'var(--color-bg-secondary)',
-        input: 'var(--color-bg-input)',
-        accent: 'var(--color-text-accent)',
-      },
+export const content = [
+  "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  "./app/**/*.{js,ts,jsx,tsx,mdx}",
+];
+export const theme = {
+  extend: {
+    backgroundColor: {
+      primary: 'var(--color-bg-primary)',
+      // added
+      primaryy: 'var(--color-bg-primaryy)',
+      // added
+      secondary: 'var(--color-bg-secondary)',
+      button: 'var(--color-bg-button)',
+    },
+    textColor: {
+      accent: 'var(--color-text-accent)',
+      primary: 'var(--color-text-primary)',
+      secondary: 'var(--color-text-secondary)',
+      btnText: 'var(--color-bg-secondary)',
+    },
+    borderColor: {
+      primary: 'var(--color-bg-primary)',
+      secondary: 'var(--color-bg-secondary)',
+      input: 'var(--color-bg-input)',
+      accent: 'var(--color-text-accent)',
     },
   },
-  plugins: [],
 };
+export const plugins = [];
